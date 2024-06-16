ae3047deb77b6f986b4d757358dfd246216b10e9
César Ng <65571974+CesarSNg@users.noreply.github.com>
2024-06-16 22:11:09 +0000

    Conexao com mongodb usando mongoose

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const authRoutes = require('./routes/authRoutes');
 const postRoutes = require('./routes/postRoutes');
 const userRoutes = require('./routes/usuarioRoutes');
 
-mongoose.connect('mongodb://localhost:27017/beatblend', {
+const app = express();
+
+mongoose.connect('mongodb+srv://csantanang:<123>@beatblender.chkkybh.mongodb.net/?retryWrites=true&w=majority&appName=BeatBlender', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB', err);
 });
 
 app.use(bodyParser.json());
@@ -16,8 +25,9 @@ app.use('/auth', authRoutes);
 app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
