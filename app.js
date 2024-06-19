b5e9a3513c2ce0fb5e87c4d949b40ee71fee33c1
Matheus Araujo Morais <matheus.araujo.morais@outlook.com>
2024-06-19 19:32:10 -0300

    Revert "Conexao com mongodb usando mongoose"
    This reverts commit ae3047deb77b6f986b4d757358dfd246216b10e9.

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
 const authRoutes = require('./routes/authRoutes');
 const postRoutes = require('./routes/postRoutes');
 const userRoutes = require('./routes/usuarioRoutes');
 
-const app = express();
-
-mongoose.connect('mongodb+srv://csantanang:<123>@beatblender.chkkybh.mongodb.net/?retryWrites=true&w=majority&appName=BeatBlender', {
+mongoose.connect('mongodb://localhost:27017/beatblend', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((err) => {
-  console.error('Error connecting to MongoDB', err);
 });
 
 app.use(bodyParser.json());
@@ -25,9 +16,8 @@ app.use('/auth', authRoutes);
 app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
 
 module.exports = app;
