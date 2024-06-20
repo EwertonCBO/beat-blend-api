90f7f39d0fba925f2e1b15fe2fbf6117cb89963f
Ewerton Carvalho <106186458+EwertonCBO@users.noreply.github.com>
2024-06-19 22:16:41 -0300

    feat: created auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/app/user/user.service';
+import { compareSync } from 'bcryptjs';
+import { UserEntity } from 'src/app/user/user.entity';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly userService: UserService, private readonly jwtService: JwtService) {}
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.userId };
+    user = await this.userService.findByEmail(user.email);
+    return {
+      token: this.jwtService.sign(payload),
+      user: user
+    };
+  }
+
+  async validateUser(email: string, password: string): Promise<any> {
+    let user: UserEntity;
+    try{
+      user = await this.userService.findByEmail(email);
+    } catch (error) {
+      return null;
+    }
+    const isPasswordValid = await compareSync(password, user.password);
+    if (!isPasswordValid) {
+      return null;
+    }
+    return user;
+  }
+}
